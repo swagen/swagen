4327d7ef7e2965ef8919559f3078c38af0282f89
Jeevan James <jeevanjj@yahoo.com>
2016-11-29 01:48:59 +0530

    Changed config file names to swagen.config.js and swagen.config.json. Removed samples.

--- cli.js
+++ cli.js
@@ -10,13 +10,12 @@ const http = require('http');
 const chalk = require('chalk');
 
 const cli = require('./lib/cli');
-const Generator = require('./lib/generator1');
 const Parser = require('./lib/parser');
 
 const currentDir = process.cwd();
 
-const configScriptFileName = 'swagenfile.js';
-const configJsonFileName = 'swagenfile.json';
+const configScriptFileName = 'swagen.config.js';
+const configJsonFileName = 'swagen.config.json';
 
 function getConfig() {
     let configScript = path.resolve(currentDir, configScriptFileName);
@@ -37,10 +36,10 @@ function processInputs(config) {
         let profile = config[profileKey];
 
         if (!profile.file && !profile.url) {
-            throw `Must specify a file or url as the swagger input for the profile '${profileKey}'`;
+            throw `[${profileKey}] Must specify a file or url as the swagger input`;
         }
         if (!profile.output) {
-            throw `Must specify an output for the profile '${profileKey}'`;
+            throw `[${profileKey}] Must specify an output file path.`;
         }
         if (!profile.debug) {
             profile.debug = {};
@@ -51,15 +50,15 @@ function processInputs(config) {
 
         if (profile.file) {
             let inputFilePath = path.resolve(currentDir, profile.file);
-            console.log(chalk.green(`Input swagger file : ${inputFilePath}`));
+            console.log(chalk.green(`[${profileKey}] Input swagger file : ${inputFilePath}`));
             fs.readFile(inputFilePath, 'utf8', function(error, swagger) {
                 if (error) {
                     throw error;
                 }
-                handleSwagger(swagger, profile);
+                handleSwagger(swagger, profile, profileKey);
             });
         } else {
-            console.log(chalk.green(`Input swagger URL : ${profile.url}`));
+            console.log(chalk.green(`[${profileKey}] Input swagger URL : ${profile.url}`));
             let swagger = '';
             let request = http.request(profile.url, function(response) {
                 response.setEncoding('utf8');
@@ -67,7 +66,7 @@ function processInputs(config) {
                     swagger += chunk;
                 });
                 response.on('end', function() {
-                    handleSwagger(swagger, profile);
+                    handleSwagger(swagger, profile, profileKey);
                 });
             });
             request.end();
@@ -78,7 +77,7 @@ function processInputs(config) {
 let config = getConfig();
 processInputs(config);
 
-function handleSwagger(swagger, profile) {
+function handleSwagger(swagger, profile, profileKey) {
     if (typeof swagger === 'string') {
         swagger = JSON.parse(swagger);
     }
@@ -88,7 +87,7 @@ function handleSwagger(swagger, profile) {
     if (profile.debug.definition) {
         let definitionJson = JSON.stringify(definition, null, 4);
         fs.writeFileSync(path.resolve(currentDir, profile.debug.definition), definitionJson, 'utf8');
-        cli.debug(`Definition file written to '${profile.debug.definition}'.`);
+        cli.debug(`[${profileKey}] Definition file written to '${profile.debug.definition}'.`);
     }
 
     let output;
@@ -97,14 +96,17 @@ function handleSwagger(swagger, profile) {
         output = generator.generate();
     } else {
         let generatorPkg = require(`swagen-${profile.generator}`);
+        if (typeof generatorPkg.validateProfile === 'function') {
+            generatorPkg.validateProfile(profile.language, profile);
+        }
         output = generatorPkg.generate(profile.language, definition, profile);
     }
 
     let outputFilePath = path.resolve(currentDir, profile.output);
     fs.writeFileSync(path.resolve(currentDir, outputFilePath), output, 'utf8');
-    console.log(chalk.green(`Code generated at '${outputFilePath}'.`))
+    console.log(chalk.green(`[${profileKey}] Code generated at '${outputFilePath}'.`))
 }
 
 module.exports = {
-    Transformer: require('./lib/generator/transformer')
+    Transformer: require('./lib/transformers/transformer')
 };
