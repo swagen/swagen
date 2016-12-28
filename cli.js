1847aea9feb23bd6033612956105a20ee7144b18
Jeevan James <jeevanjj@live.com>
2016-12-29 05:18:20 +0530

    Renamed default to generate Updated help content

--- cli.js
+++ cli.js
@@ -6,10 +6,15 @@ const process = require('process');
 const minimist = require('minimist');
 const _ = require('lodash');
 
+const swagenCore = require('swagen-core');
+const cli = swagenCore.cli;
+
+const helpCommand = require('./lib/help');
+
 let allArgs = process.argv;
 let argsCount = _.endsWith(allArgs[0].toLowerCase(), 'node.exe') ? 3 : 2;
 
-let command = 'default';
+let command = 'generate';
 let argv = {};
 if (allArgs.length >= argsCount) {
     command = allArgs[argsCount - 1];
@@ -20,7 +25,8 @@ let commandModule;
 try {
     commandModule = require(`./lib/${command}`);
 } catch (ex) {
-    console.error(`Invalid command: '${command}'`);
+    cli.error(`Invalid command: '${command}'`);
+    helpCommand();
 }
 if (commandModule) {
     commandModule(argv);
