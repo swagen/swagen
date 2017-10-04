5c2ea4af2cb13308ffb792f2936eb844ced991e4
Jeevan James <jeevanjj@live.com>
2017-10-04 20:56:03 +0530

    Moved some common code to the utils.js file.

--- cli.js
+++ cli.js
@@ -3,15 +3,19 @@
 'use strict';
 
 const process = require('process');
-const chalk = require('chalk');
+
 const minimist = require('minimist');
 const _ = require('lodash');
 
+const utils = require('./lib/utils');
+const cli = utils.cli;
 const helpCommand = require('./lib/help');
 
 const allArgs = process.argv;
 const argsCount = _.endsWith(allArgs[0].toLowerCase(), 'node.exe') ? 3 : 2;
 
+// Figure out the command and parse the remaining arguments.
+// If no command is specified, assume it to be generate.
 let command = 'generate';
 let argv = {};
 if (allArgs.length >= argsCount) {
@@ -19,27 +23,35 @@ if (allArgs.length >= argsCount) {
     argv = minimist(allArgs.slice(argsCount));
 }
 
+// Load the module for the command.
 let commandModule;
 try {
     commandModule = require(`./lib/${command}`);
 } catch (ex) {
-    console.log(chalk.red(`Invalid command: '${command}'`));
-    console.log(chalk.red(ex));
+    // If the module could not be loaded, it means that the specified command is invalid.
+    // Display error message and help.
+    cli.error(`Invalid command: '${command}'`);
+    cli.error(ex);
     helpCommand();
 }
-if (commandModule) {
-    try {
-        commandModule(argv);
-    } catch (ex) {
-        if (ex instanceof Error) {
-            console.log(chalk.red(ex.message));
-        } else {
-            console.log(chalk.red(ex));
-        }
-        console.log();
-        const helpArgs = {
-            _: [command]
-        };
-        helpCommand(helpArgs);
+
+if (!commandModule || typeof commandModule !== 'function') {
+    cli.error(`An unexpected error occurred; could not load the command '${command}'.`);
+}
+
+try {
+    // Run the command
+    commandModule(argv);
+} catch (ex) {
+    // If the command throws an exception, display the error message and command help.
+    if (ex instanceof Error) {
+        cli.error(ex.message);
+    } else {
+        cli.error(ex);
     }
+    console.log();
+    const helpArgs = {
+        _: [command]
+    };
+    helpCommand(helpArgs);
 }
